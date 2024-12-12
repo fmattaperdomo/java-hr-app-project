eb416a62b414e87254f50962d421dc54ce5f6529
fmattaperdomo <fmattaperdomo@gmail.com>
2024-12-11 20:08:46 -0500

    InitialVersion

---
+++ src/test/java/com/fmattaperdomo/rh_app/RhAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmattaperdomo.rh_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
